1165f8df43434fd278954145cd06aea564ec34f2
肖镇龙 <xiaozhenlong@xiaozhenlongdeMacBook-Air.local>
2015-08-21 22:52:31 +0800

    Initial Commit

---
+++ Test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Test
+//
+//  Created by 肖镇龙 on 15/8/21.
+//  Copyright (c) 2015年 肖镇龙. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
