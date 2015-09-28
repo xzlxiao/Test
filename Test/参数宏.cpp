465e6fc0456986f57bb30c82380fb811f823f115
xzlxiao <xzlxiao@outlook.com>
2015-09-28 19:10:16 +0800

    参数宏

---
+++ Test/参数宏.cpp
@@ -0,0 +1,12 @@
+//²âÊÔ²ÎÊıºê
+#define pi 3.1415926
+#define roufac(x) ((1.0/2.0)*(pi)*(x))
+#include <iostream>
+using namespace std;
+int main()
+{
+	double a;
+	a = roufac (10);
+	cout << a << endl;
+	return 0;
+}
