792b39cce070511cabfd758e50750d2319a08a3f
xzlxiao <xzlxiao@outlook.com>
2015-12-06 11:42:15 +0800

    python2.7文件夹初始化

---
+++ 2/二进制.cpp
@@ -0,0 +1,8 @@
+// ¶ş½øÖÆ²âÊÔ
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << 0b10 << endl;
+	return 0;
+}
---
+++ python2.7/HelloWorld.py
@@ -0,0 +1,3 @@
+# -*- coding: cp936 -*-
+# hello world 
+print 'hello world'
