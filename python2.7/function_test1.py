054cbff21ade1d1af916d78dcca0a6a8ba65de7c
肖镇龙 <xzlxiao@outlook.com>
2016-01-06 21:47:55 +0800

    2016-1-6

---
+++ python2.7/function_test1.py
@@ -0,0 +1,5 @@
+# -*- coding: cp936 -*-
+def test():
+	print 'This is print'
+	return
+	print 'This is not'
---
+++ python2.7/测试分支语句.py
@@ -0,0 +1,13 @@
+# -*- coding: cp936 -*-
+'''
+²âÊÔ·ÖÖ§Óï¾ä
+'''
+
+num = input('Enter a number: ')
+if num > 0:
+    print 'The number is positive'
+elif num < 0:
+	print 'The number is negative'
+else:
+	print 'The Number is zero'
+		
